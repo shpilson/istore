850e76aca5354ffdca8d67aafdff0ed2717241cb
shpilson <kovalenko.jora@gmail.com>
2021-06-09 22:01:24 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,10 @@ const header = document.querySelector(".container-fluid");
 function changeHeader (el) {
     if (el == 1) {
         el.div = ".container";
-    };
+    }
+    else {
+        console.log("Менять нечего")
+    }
 }
 
 changeHeader(header);
