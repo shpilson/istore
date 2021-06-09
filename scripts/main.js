4d465770680738ecce39ff9e805927ad64e4f6e9
shpilson <kovalenko.jora@gmail.com>
2021-06-09 22:26:03 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 setTimeout(function changeHeader () {
         console.log("Поймали элемент");
-        const old = document.querySelector('.container-fluid').className = 'container';
+        document.querySelector('.container-fluid').className = 'container';
 }, 1000);
 
 changeHeader();
