d6931c5d5b95acadbd915c897754d04d2e820cc8
shpilson <kovalenko.jora@gmail.com>
2021-06-11 15:43:45 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,17 @@
 // Сужаем верхнюю шапку, меняя класс
 
-setTimeout(function changeHeader () {
-        document.querySelector('.container-fluid').className = 'container';
+setTimeout(function changeHeader() {
+    document.querySelector('.container-fluid').className = 'container';
 }, 100);
+
+
+// Навигация -> Перезвоните
+
+const nav = document.querySelector(".nav.sf-menu");
+
+function addNavContacts() {
+    advantage1.insertAdjacentHTML('afterend', `
+    <div class="nav_contacts">
+    <a href="tel:null" role="link">+7 (968)<b>327-27-00</b></a>
+    </div>`)
+};
