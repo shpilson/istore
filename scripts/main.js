877fd72b48f73946b4a33d87df1828280ba040be
shpilson <kovalenko.jora@gmail.com>
2021-06-20 19:12:42 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -103,7 +103,7 @@ setTimeout(function socialFooter() {
 setTimeout(function widgetHeader() {
     const widgetSelector = document.querySelector('.header_widgets')
     widgetSelector.insertAdjacentHTML('afterbegin',`
-        <div class="header_wishlist">
+        <div class="header_login">
             <a class="d-none d-sm-block d-lg-none" href="https://store110910.sellavi.com/index.php?route=account/wishlist" role="link">
             <i class="far fa-heart">::before</i>
             </a>
