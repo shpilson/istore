dfa15da6bb27990a51702cd8f483802242ad537f
shpilson <kovalenko.jora@gmail.com>
2021-06-20 18:04:49 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -76,3 +76,25 @@ setTimeout(function addYandexMap() {
 </div>
   </div>`)
 }, 100);
+
+
+
+// Социальные сети в футере
+
+setTimeout(function socialFooter() {
+    const socialIconSelector = document.querySelector(".footer .footer-widgets .container .row");
+    socialIconSelector.insertAdjacentHTML('beforeend', 
+    `<div class="col-md-3">
+        <div class="widget widget-categories">
+            <h4 class="widget-title">Мы в соцсетях</h4>
+                <ul class="social-icons-new">
+                    <li>
+                        <a href="instagram.com/aquarel" target="_blank" class="instagram" data-gtm_label="instagram" title="instagram_title" role="link">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </li>
+                </ul>
+        </div>
+    </div>`)
+}, 100);
+
